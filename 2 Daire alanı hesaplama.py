4998c5323baed74c0bd549e7ed942490bfabec29
omerdurmus61 <59428294+omerdurmus61@users.noreply.github.com>
2020-09-16 09:30:09 +0300

    Add files via upload

---
+++ 2 Daire alanı hesaplama.py
@@ -0,0 +1,23 @@
+
+# -------Dairenin cevresini hesaplayan program.-------
+
+#pi sabiti tanımlandı.
+pi=3.14
+
+#r değeri için ekrana çıktı verildi.
+print("Daire yarı capını giriniz.")
+
+#r değeri alındı.
+r = input("r: ")
+
+#r değeri int'e çevrildi.
+r = int(r)
+
+#cevre hesaplandı
+cevre = 2*pi*r
+
+#cevre bilgisi ekrana yazdırıldı.
+print("cevre=2*pi*r") 
+
+print(cevre)
+
