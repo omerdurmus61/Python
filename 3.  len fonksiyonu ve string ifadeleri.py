467388a804b4ad72381e1b518233c2a0b6c180b4
omerdurmus61 <59428294+omerdurmus61@users.noreply.github.com>
2020-09-16 09:42:57 +0300

    Add files via upload

---
+++ 3.  len fonksiyonu ve string ifadeleri.py
@@ -0,0 +1,13 @@
+x="Merhaba Dünya"
+
+print(x)
+
+uzunluk = len(x)
+
+print("String'in uzunluğu")
+print(uzunluk)
+
+#print fonksiyonu içersinde farkı kullanımlar
+
+print("Merhaba\nDünya") #\n new line anlamına gelir yeni satıra geçilir.
+print("Merhaba\tDünya") #\t bir tab kadar boşluk bırakır.
