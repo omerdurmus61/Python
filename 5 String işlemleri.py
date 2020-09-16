5fe52b6f132afbda3e7c55e42c3f2b801c7a2c01
omerdurmus61 <59428294+omerdurmus61@users.noreply.github.com>
2020-09-16 15:37:44 +0300

    Add files via upload

---
+++ 5 String işlemleri.py
@@ -0,0 +1,21 @@
+#Sting özellikleri
+
+isim= "omer"
+
+#capitalize() özelliği stringin ilk harfini büyük harf yapar.
+isim = isim.capitalize()
+
+print(isim)
+
+tamIsim="omer can durmus"
+
+#split() özelliği boşluk karakterine göre cümleyi kelimelere ayırır.
+print(tamIsim.split())
+
+#upper özelliği bütün karakterleri büyük yapar.
+print(tamIsim.upper())
+#Stringleri birleştirme.
+ad="omer can"
+soyad=" durmus"
+print(ad+""+soyad)
+print(ad+soyad)
