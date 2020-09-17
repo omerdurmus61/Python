44fc67bdc507e9f822e81524e40df217a430b914
omerdurmus61 <59428294+omerdurmus61@users.noreply.github.com>
2020-09-17 17:33:28 +0300

    Add files via upload

---
+++ 13 Kontroller ve karşılaştırma operatörleri.py
@@ -0,0 +1,32 @@
+x=5
+y=4
+print(x>y)
+print(x<y)
+
+print(x>=y)
+print(x<=y)
+
+print(x<=5)
+print(x>=5.0)
+print(x>=5.1)
+
+a=61
+b=61
+print(a==b)
+
+b=16
+
+print(a!=b)
+
+x=1
+y=2
+a=10
+b=20
+
+print(x<y and a<b)
+
+print(x>2 and a<b)
+
+print(x<y or a<b)
+
+print(x<y or a>b)
---
+++ 14 Koşullar.py
@@ -0,0 +1,16 @@
+#if & else & elif
+
+x= input("x sayısını giriniz:")
+y= input("y sayısını giriniz:")
+
+if x>y:
+    print("x daha büyük")
+elif x==y:
+    print("x ile y eşit")
+else :
+    print("y daha büyük")
+
+   
+
+
+    
