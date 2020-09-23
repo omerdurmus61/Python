6ee9f569079ea70e5b3a281635194ca211c89944
omerdurmus61 <59428294+omerdurmus61@users.noreply.github.com>
2020-09-23 19:02:41 +0300

    Add files via upload

---
+++ 37 python özel operatörleri.py
@@ -0,0 +1,25 @@
+x = y =[1,2,3]
+z=[1,2,3]
+
+print(x == y)
+print(x == z)
+
+print(x is y)
+print(x is z)
+
+a =[1,2,3]
+b=[2,4]
+c=[1,2,3]
+
+print(a is b)
+print(a is c)
+print(a is not c)
+
+x =["Bursa","Istanbul"]
+
+print("Bursa" in x)
+
+name ="omer"
+
+print( "a" not in name)
+print("r" in name)
